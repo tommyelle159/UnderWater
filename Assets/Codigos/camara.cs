096dd5ed9879728f0c5743aabc30168260200f8a
tommyelle159 <ar.tommaso.langiu@gmail.com>
2023-05-09 13:10:30 +0200

    first commit

---
+++ Assets/Codigos/camara.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara : MonoBehaviour
+{
+    public Transform personaje;
+    public float dondepersonaje;
+    public float dondepersonajeY;
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        dondepersonaje = personaje.transform.position.x;
+        dondepersonajeY = personaje.transform.position.y;
+        transform.position = new Vector3 (dondepersonaje, dondepersonajeY, -10);
+        
+    }
+}
