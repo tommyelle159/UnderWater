0ed0dcbdd588d9dafeb67048db0534ea3b32cec0
paupueab <paulapuebla8@gmail.com>
2023-05-25 20:22:25 +0200

    //He puesto apuntes en el ScriptCommit 25 mayo

--- Assets/Codigos/CodigoCommit.cs
+++ Assets/Codigos/CodigoCommit.cs
@@ -12,6 +12,15 @@ public class CodigoCommit : MonoBehaviour
 
 //actualizacion 25 de mayo
 //MIRAR TAREAS CON LA PROGRAMACION QUE FALLA
+// He añadido los enemigos pero no mueren con bala agua
+// diseño de nivel 1 y 2 ok
+// Menú y botón de empezar ok
+// animación en la pipa, no desaparece en clase se pregunta
+//animación de salto no funciona
+//cinemachine sigue en el nivel 2 ok 
+//bombona de oxigeno ok
+//Falta asignar Script tesoro a Zanahoria
+//Incorporar botones menú
 
 {
     // Start is called before the first frame update
